b5ae45c95f1abfe321df7afa6bae102c19bec1c1
mmorangarcia <martin.moran.garcia.1996@gmail.com>
2026-02-16 01:24:23 +0100

    add animation

--- src/components/Experience/ProjectsCard.tsx
+++ src/components/Experience/ProjectsCard.tsx
@@ -1,5 +1,6 @@
 import type { Project } from "@/types";
 import SpotlightCard from "./SpotlightCard";
+import { InView } from "../ui/in-view";
 
 type ProjectProps = {
   project: Project;
@@ -8,16 +9,25 @@ export default function ProjectsCard({ project }: ProjectProps) {
   return (
     <>
       <div className="p-2">
-        <SpotlightCard
-          className="custom-spotlight-card"
-          spotlightColor="rgba(0, 229, 255, 0.2)"
+        <InView
+          variants={{
+            hidden: { opacity: 0, y: 100, filter: "blur(4px)" },
+            visible: { opacity: 1, y: 0, filter: "blur(0px)" },
+          }}
+          viewOptions={{ margin: "0px 0px -200px 0px" }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
         >
-          <section className="flex justify-between align-middle">
-            <h1 className="text-3xl pb-5 font-bold">{project.title}</h1>
-            <p>{project.period}</p>
-          </section>
-          <span>{project.description}</span>
-        </SpotlightCard>
+          <SpotlightCard
+            className="custom-spotlight-card"
+            spotlightColor="rgba(0, 229, 255, 0.2)"
+          >
+            <section className="flex justify-between align-middle">
+              <h1 className="text-3xl pb-5 font-bold">{project.title}</h1>
+              <p>{project.period}</p>
+            </section>
+            <span>{project.description}</span>
+          </SpotlightCard>
+        </InView>
       </div>
     </>
   );
